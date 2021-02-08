a1e5d9dc6324f43a18d9ac1d29fe1e2fe276aea8
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2021-02-08 12:04:55 -0800

    add week 5 notes

---
+++ class_notes/week_6/conversions.py
@@ -0,0 +1,11 @@
+# conversions.py
+
+# a function to convert between teaspoons and table spoons
+def tea_to_tab(teas):
+    tabs = teas/3
+    return tabs
+
+# a function to convert between cups and pints
+def cup_to_pt(cups):
+    pints = cups/2
+    return pints
---
+++ class_notes/week_6/my_funcs.py
@@ -0,0 +1,12 @@
+# my_funcs.py
+
+# create a function to add 2 to any number
+def plus_2(n):
+    out = n + 2
+    return out
+
+
+# create a function to add 3 to any number
+def plus_3(x):
+    ans = x + 3
+    return ans
